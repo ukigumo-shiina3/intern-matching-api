686c9ff87bc7acf6cf14096bec26b600b57f1c3f
ukigumo-shiina3 <daisukeyao@outlook.jp>
2025-10-25 12:28:59 +0900

    refactor: 💡 optional: trueに変更

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
-  belongs_to :intern
-  belongs_to :company
+  belongs_to :intern, optional: true
+  belongs_to :company, optional: true
   has_many :messages, dependent: :destroy
 
   validates :intern_id, presence: true
