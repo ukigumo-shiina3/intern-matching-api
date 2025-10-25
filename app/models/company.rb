9707474a01d4ccb588ebddbff1f867da2bfb279c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2025-10-25 15:18:45 +0900

    refactor: 💡 アソシエーションの追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ApplicationRecord
+  has_many :rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :interns, through: :rooms
 end
--- app/models/intern.rb
+++ app/models/intern.rb
@@ -1,2 +1,5 @@
 class Intern < ApplicationRecord
+  has_many :rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :companies, through: :rooms
 end
