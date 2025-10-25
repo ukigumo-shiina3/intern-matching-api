f88f90ab6ecbebaf2221217390f3d85793c48327
ukigumo-shiina3 <daisukeyao@outlook.jp>
2025-10-25 20:00:49 +0900

    feat: 🎸 メソッドの追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,27 @@
 class Message < ApplicationRecord
-  belongs_to :intern
-  belongs_to :company
   belongs_to :room
+  belongs_to :intern, optional: true
+  belongs_to :company, optional: true
 
   validates :content, presence: true
+  validate :sender_must_be_exactly_one
+
+
+  def mark_as_read!
+    update(read_at: Time.current) if read_at.nil?
+  end
+
+  def sender
+    intern || company
+  end
+
+  private
+
+  def sender_must_be_exactly_one
+    if intern_id.blank? && company_id.blank?
+     errors.add(:base, "送信者が必要です")
+    elsif intern_id.present? && company_id.present?
+      errors.add(:base, "送信者は1人だけです")
+    end
+  end
 end
