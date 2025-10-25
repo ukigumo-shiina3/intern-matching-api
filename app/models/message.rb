73d702493466e5caead01fbba3fd2429153f92ae
ukigumo-shiina3 <daisukeyao@outlook.jp>
2025-10-25 11:34:58 +0900

    refactor: 💡 アソシエーションとバリデーションを定義

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+  belongs_to :intern
+  belongs_to :company
+  belongs_to :room
+
+  validates :content, presence: true
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,9 @@
 class Room < ApplicationRecord
+  belongs_to :intern
+  belongs_to :company
+  has_many :messages, dependent: :destroy
+
+  validates :intern_id, presence: true
+  validates :company_id, presence: true
+  validates :intern_id, uniqueness: { scope: :company_id }
 end
