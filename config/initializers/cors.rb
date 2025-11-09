f3a82218b5a7554be806a1d6cfc2df7839bab9bb
ukigumo-shiina3 <daisukeyao@outlook.jp>
2025-11-09 09:57:15 +0900

    feat: 🎸 デプロイ先のフロントエンドのURLを設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://localhost:3001", "https://https://intern-matching-api.onrender.com.onrender.com"
+    # Development
+    origins "http://localhost:3000",
+            "http://localhost:3001",
+            # Production API URL
+            "https://intern-matching-api.onrender.com",
+            # Production Frontend URL
+            ENV.fetch("FRONTEND_URL", "https://intern-matching-web.vercel.app")
 
     resource "*",
       headers: :any,
