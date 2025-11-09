970e91668077d76e17909a5766215fd5cd416a95
ukigumo-shiina3 <daisukeyao@outlook.jp>
2025-11-09 13:01:38 +0900

    refactor: 💡 不要なコメント削除

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # Development
     origins "http://localhost:3000",
             "http://localhost:3001",
-            # All Vercel deployments (production + preview)
             /https:\/\/.*\.vercel\.app$/
 
     resource "*",
