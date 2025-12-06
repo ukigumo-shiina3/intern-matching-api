99d1cab0b69e726bc6f3825067217d937bfcf712
ukigumo-shiina3 <daisukeyao@outlook.jp>
2025-12-06 10:50:14 +0900

    feat: 🎸 アソシエーションとバリデーション追加

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,8 @@
 class Entry < ApplicationRecord
+  belongs_to :intern
+  belongs_to :job
+
+  validates :intern_id, presence: true
+  validates :job_id, presence: true
+  validates :current_status, presence: true
 end
--- app/models/intern.rb
+++ app/models/intern.rb
@@ -2,4 +2,6 @@ class Intern < ApplicationRecord
   has_many :rooms, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :companies, through: :rooms
+  has_many :entries, dependent: :destroy
+  has_many :jobs, through: :entries
 end
